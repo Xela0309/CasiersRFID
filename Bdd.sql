5f62027b150be8d9f42a66a3c573b8cd27752794
Alexandre PICARD <picarda@TURGOT.LAN>
2025-02-03 11:49:41 +0100

    Modification des Tables

--- Bdd.sql
+++ Bdd.sql
@@ -1,28 +1,41 @@
 
 DROP TABLE IF EXISTS Casier;
 DROP TABLE IF EXISTS Utilisateur;
+DROP TABLE IF EXISTS Tag;
+DROP TABLE IF EXISTS Affectation;
+DROP TABLE IF EXISTS Administrateur;
 
 CREATE TABLE Tag (
     id_Tag INT PRIMARY KEY AUTO_INCREMENT,
-    tag VARCHAR(50) NOT NULL UNIQUE,
-    dateDebut DATE NOT NULL,
-    dateFin DATE NOT NULL
+    tag VARCHAR(50) NOT NULL 
 ) ENGINE = InnoDB;
 
 CREATE TABLE Utilisateur (
     id_Utilisateur INT PRIMARY KEY AUTO_INCREMENT,
-    tag VARCHAR(50) NOT NULL UNIQUE,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    motDePasse VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_Utilisateur FOREIGN KEY (tag) REFERENCES Tag(tag)
+    email VARCHAR(50) NOT NULL 
 ) ENGINE=InnoDB;
 
 CREATE TABLE Casier (
     id_Casier INT PRIMARY KEY AUTO_INCREMENT,
+    numeroCasier INT NOT NULL 
+) ENGINE=InnoDB;
+
+CREATE TABLE Affectation (
+    id_Affectation INT PRIMARY KEY AUTO_INCREMENT,
+    id_Casier INT NOT NULL,
     id_Utilisateur INT NOT NULL,
-    numeroCasier INT NOT NULL UNIQUE,
-    tag VARCHAR(50),
-    CONSTRAINT fk_id_Utilisateur FOREIGN KEY (id_Utilisateur) REFERENCES Utilisateur(id_Utilisateur)
+    id_Tag INT NOT NULL,
+    dateDebut DATE NOT NULL,
+    dateFin DATE NOT NULL,
+    CONSTRAINT fk_id_Casier FOREIGN KEY (id_Casier) REFERENCES Casier(id_Casier),
+    CONSTRAINT fk_id_Utilisateur FOREIGN KEY (id_Utilisateur) REFERENCES Utilisateur(id_Utilisateur),
+    CONSTRAINT fk_id_Tag FOREIGN KEY (id_Tag) REFERENCES Tag(id_Tag)
+) ENGINE=InnoDB;
+
+CREATE TABLE Admistrateur (
+    id_Administrateur INT PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB;
