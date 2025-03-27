a4bd962eb7f3b4e1a8f7cf547704d24a1889b9e7
Indotonyo <121133784+Indotonyo@users.noreply.github.com>
2025-03-27 08:52:29 +0100

    Update Requete.sql

--- Requete.sql
+++ Requete.sql
@@ -7,10 +7,11 @@ INSERT INTO `Casier`(`numeroCasier`) VALUES (2);
 INSERT INTO `Casier`(`numeroCasier`) VALUES (3);
 INSERT INTO `Casier`(`numeroCasier`) VALUES (4);
 
-INSERT INTO `Tag`(`tag`) VALUES ("[01][01][01][01][01]");
-INSERT INTO `Tag`(`tag`) VALUES ("[01][01][01][01][02]");
-INSERT INTO `Tag`(`tag`) VALUES ("[01][01][01][01][03]");
-INSERT INTO `Tag`(`tag`) VALUES ("[01][01][01][01][04]");
+INSERT INTO `Tag`(`tag`) VALUES ("010C2E14093E");
+INSERT INTO `Tag`(`tag`) VALUES ("2600B3271BA9");
+INSERT INTO `Tag`(`tag`) VALUES ("2600B43EFF53");
+INSERT INTO `Tag`(`tag`) VALUES ("2600B438C16B");
+INSERT INTO `Tag`(`tag`) VALUES ("2600B326FE4D");
 
 INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "e3ca2936831f30","e4ca2b3b861738", "ffe00b", "f2e66a62db4078d39a");
 INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "f0c2293e81","f2cd3338861830", "fbe108", "f7e46a66da4f78d396");
