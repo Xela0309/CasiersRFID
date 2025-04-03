38aaf565940966e407e10d1596f2574fe22fa39e
Picard Alexandre <130000388+Xela0309@users.noreply.github.com>
2025-04-03 14:24:50 +0200

    Update Bdd.sql

--- Bdd.sql
+++ Bdd.sql
@@ -4,11 +4,14 @@ DROP TABLE IF EXISTS Casier;
 DROP TABLE IF EXISTS Visiteur;
 DROP TABLE IF EXISTS Tag;
 
-
-
 CREATE TABLE Tag (
     tag VARCHAR(100) PRIMARY KEY NOT NULL 
+    etat Enum('O','U','P') NOT NULL,
 ) ENGINE = InnoDB;
+
+# O = occupée
+# U = utilisable
+# P = perdu
 
 CREATE TABLE Visiteur (
     id_Visiteur INT PRIMARY KEY AUTO_INCREMENT,
