4216d411bf0ae38357083ab9a621860f2bbeb7c7
Picard Alexandre <130000388+Xela0309@users.noreply.github.com>
2025-04-10 18:10:58 +0200

    Update Requete.sql

--- Requete.sql
+++ Requete.sql
@@ -13,11 +13,8 @@ INSERT INTO `Tag`(`tag`,`etat`) VALUES ("2600B43EFF53","U");
 INSERT INTO `Tag`(`tag`,`etat`) VALUES ("2600B438C16B","U");
 INSERT INTO `Tag`(`tag`,`etat`) VALUES ("2600B326FE4D","U");
 
-INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "e3ca2936831f30","e4ca2b3b861738", "ffe00b", "f2e66a62db4078d39a");
-INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "f0c2293e81","f2cd3338861830", "fbe108", "f7e46a66da4f78d396");
-INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "f4d622258d1734","e7ca33389a173b", "f6e60b01", "f6e56a60da4578c69b");
-INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "e1c629369a12","fec2333f8605", "e1c629369a1a21", "f5f96a60d74378d49f");
-
+INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "e3ca2936831f30","e4ca2b3b861738", "ffe00b", "f2e66a62db4078d39a","f2cf2b32821732f9b8");
+INSERT INTO `Visiteur`(`nom`, `prenom`, `compagnie`, `numPlaque`) VALUES ( "f0c2293e81","f2cd3338861830", "fbe108", "f7e46a66da4f78d396","e1cc3e369a1b30ba88d776");
 
 INSERT INTO `Affectation`(`id_Casier`, `id_Visiteur`, `id_Tag`, `dateDebut`, `dateFin`) VALUES (1, 1, "010C2E14093E", "2020-01-01", "2020-01-02");
 INSERT INTO `Affectation`(`id_Casier`, `id_Visiteur`, `id_Tag`, `dateDebut`, `dateFin`) VALUES (2, 2, "2600B3271BA9", "2020-01-01", "2020-01-02");
